da1a14971f7f23472a2e430b59fa90f737eb1aaf
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-04 15:01:55 -0800

    Create helloworld.sh
    Just a hello world script in Bash.

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+# Just to update my hello world repository with a proper Bash script
+# Before I go insane
+
+# Hello World Script
+echo "Hello World!"
+
+# Configure the script
+chmod a+x helloworld.sh
+bash helloworld.sh
